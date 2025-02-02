c115d9946fd6067ae7cdb0edde6b277c25e1e31c
Oba The Great <105570560+ObaTheGreat@users.noreply.github.com>
2025-02-01 22:35:20 -0500

    IOS Only

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,15 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.app',
+  id: 'org.nativescript.shrtr',
   appPath: 'app',
   appResourcesPath: '../../tools/assets/App_Resources',
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
+  },
+  ios: {
+    discardUncaughtJsExceptions: true,
+    SPMPackages: []
   }
 } as NativeScriptConfig;
